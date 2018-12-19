5eded272114baa2ef16839f2e82934dc791dcf2b
Deyan Ginev <d.ginev@jacobs-university.de>
2018-12-19 13:15:12 -0500

    flesh out ~ binding

--- rtx/src/package/pool/tex_appendix_b_p359.rs
+++ rtx/src/package/pool/tex_appendix_b_p359.rs
@@ -2,5 +2,68 @@ use crate::package::*;
 
 pub fn load_definitions(state: &mut State) -> Result<()> {
   SetupBindingMacros!(state);
+
+  //======================================================================
+  // TeX Book, Appendix B. p. 359
+
+  // Ah, since \ldots can appear in text and math....
+
+  DefConstructor!(
+    "\\ldots",
+    "?#isMath(<ltx:XMTok name='ldots' font='#font' role='ID'>\u{2026}</ltx:XMTok>)(\u{2026})"
+  );
+  // TODO
+  // properties => properties!(sub[stomach, args, state] {
+  //   if state.lookup_bool("IN_MATH") {
+  //     font => state.lookup_font().merge(family => "serif", series => "medium", shape =>
+  // "upright").specialize("\u{2026}")   }
+  //  })
+  // Since not DefMath!
+
+  // And so can \vdots
+  // DefConstructorI('\vdots', undef,
+  //   "?#isMath(<ltx:XMTok name='vdots' font='#font' role='ID'>\x{22EE}</ltx:XMTok>)(\x{22EE})",
+  //   properties => sub {
+  //     (LookupValue('IN_MATH')
+  //       ? (font => LookupValue('font')->merge(family => 'serif',
+  //           series => 'medium', shape => 'upright')->specialize("\x{22EE}"))
+  //       : ()); });    # Since not DefMath!
+  //                     # But not these!
+  // DefMathI('\cdots', undef, "\x{22EF}", role => 'ID');    # MIDLINE HORIZONTAL ELLIPSIS
+
+  // DefMathI('\ddots', undef, "\x{22F1}", role => 'ID');           # DOWN RIGHT DIAGONAL ELLIPSIS
+  // DefMathI('\colon', undef, ':',        role => 'METARELOP');    # Seems like good default role
+  //         # Note that amsmath redefines \dots to be `smart'.
+  //         # Aha, also can be in text...
+  // DefConstructorI('\dots', undef,
+  //   "?#isMath(<ltx:XMTok name='dots' font='#font' role='ID'>\x{2026}</ltx:XMTok>)(\x{2026})",
+  //   properties => sub {
+  //     (LookupValue('IN_MATH')
+  //       ? (font => LookupValue('font')->merge(family => 'serif',
+  //           series => 'medium', shape => 'upright')->specialize("\x{2026}"))
+  //       : ()); });    # Since not DefMath!
+
+  // And while we're at it...
+
+  // DefMathLigature("\u{22C5}\u{22C5}\u{22C5}" => "\u{22EF}", role => 'ID', name => 'cdots');
+
+  DefLigature!(r"[.][.][.]", "\u{2026}", fontTest => sub[arg] {arg.get_family() != Some("typewriter".into()) });  // ldots
+
+  // TODO:
+  // DefMathLigature("..." => "\x{2026}", role => 'ID', name => 'ldots');
+  DefLigature!(r"--", "\u{2013}",fontTest => sub[arg] {arg.get_family() != Some("typewriter".into()) }); // EN DASH (NOTE: With digits before &
+                                   //, aft => \N{FIGURE DASH})
+  DefLigature!(r"---", "\u{2014}", fontTest => sub[arg] {arg.get_family() != Some("typewriter".into()) }); // EM DASH
+                                    // Ligatures for doubled single left & right quotes to convert to double quotes
+                                    // [should ligatures be part of a font, in the first place? (it is in TeX!)
+  DefLigature!("\u{2018}\u{2018}", "\u{201C}", fontTest => sub[arg] {
+    if arg.get_family() != Some("typewriter".into()) {
+      let encoding = arg.get_encoding().unwrap_or(Cow::Borrowed("OT1"));
+      encoding == "OT1" || encoding == "T1" } else {false} });
+  DefLigature!("\u{2019}\u{2019}", "\u{201D}", fontTest => sub[arg] {
+    if arg.get_family() != Some("typewriter".into()) {
+      let encoding = arg.get_encoding().unwrap_or(Cow::Borrowed("OT1"));
+      encoding == "OT1" || encoding == "T1" } else {false} });
+
   Ok(())
 }
---
+++ rtx/src/package/pool/tex_appendix_b_p364.rs
@@ -0,0 +1,144 @@
+use crate::package::*;
+
+//======================================================================
+// TeX Book, Appendix B. p. 364
+pub fn load_definitions(state: &mut State) -> Result<()> {
+  SetupBindingMacros!(state);
+
+// Let's hope nobody is messing with the output routine...
+// DefPrimitiveI('\footnoterule', undef, undef);
+
+// #======================================================================
+// # End of TeX Book definitions.
+// #======================================================================
+
+// #**********************************************************************
+// # Stray stuff .... where to ?
+// #**********************************************************************
+
+// # Mostly ignorable, although it could add an attribute to an ancestor
+// # to record the desired justification.
+// # Spacing stuff
+// DefConstructor('\@', '');
+// # Math spacing.
+
+// # Math style.
+// # Also record that this explicitly sets the mathstyle (support for \over, etal)
+// DefPrimitiveI('\displaystyle', undef, sub {
+//     MergeFont(mathstyle => 'display');
+//     Box(undef, undef, undef, T_CS('\displaystyle'), explicit_mathstyle => 1); });
+// DefPrimitiveI('\textstyle', undef, sub {
+//     MergeFont(mathstyle => 'text');
+//     Box(undef, undef, undef, T_CS('\textstyle'), explicit_mathstyle => 1); });
+// DefPrimitiveI('\scriptstyle', undef, sub {
+//     MergeFont(mathstyle => 'script');
+//     Box(undef, undef, undef, T_CS('\scriptstyle'), explicit_mathstyle => 1); });
+// DefPrimitiveI('\scriptscriptstyle', undef, sub {
+//     MergeFont(mathstyle => 'scriptscript');
+//     Box(undef, undef, undef, T_CS('\scriptscriptstyle'), explicit_mathstyle => 1); });
+
+// #======================================================================
+
+// # Special Characters.
+// # Try to give them some sense in math...
+// DefMacroI('\#',   undef, '\ifmmode\lx@math@hash\else\lx@text@hash\fi');
+// DefMacroI('\&',   undef, '\ifmmode\lx@math@amp\else\lx@text@amp\fi');
+// DefMacroI('\%',   undef, '\ifmmode\lx@math@percent\else\lx@text@percent\fi');
+// DefMacroI("\\\$", undef, '\ifmmode\lx@math@dollar\else\lx@text@dollar\fi');
+// DefMacroI('\_',   undef, '\ifmmode\lx@math@underscore\else\lx@text@underscore\fi');
+// DefPrimitiveI('\lx@text@hash',       undef, '#',  alias => '\#');
+// DefPrimitiveI('\lx@text@amp',        undef, '&',  alias => '\&');
+// DefPrimitiveI('\lx@text@percent',    undef, '%',  alias => '\%');
+// DefPrimitiveI('\lx@text@dollar',     undef, "\$", alias => "\\\$");
+// DefPrimitiveI('\lx@text@underscore', undef, '_',  alias => '\_');
+// DefMathI('\lx@math@hash',    undef, '#', alias => '\#');
+// DefMathI('\lx@math@amp',     undef, '&', role  => 'ADDOP', meaning => 'and', alias => '\&');
+// DefMathI('\lx@math@percent', undef, '%', role  => 'POSTFIX', meaning => 'percent', alias => '\%');
+// DefMathI('\lx@math@dollar', undef, "\$", role => 'OPERATOR', meaning => 'currency-dollar',
+//   alias => "\\\$");
+// DefMathI('\lx@math@underscore', undef, '_', alias => '\_');
+
+// # Discretionary times; just treat as invisible ?
+// DefMathI('\*', undef, "\x{2062}", role => 'MULOP', name => '', meaning => 'times'); # INVISIBLE TIMES (or MULTIPLICATION SIGN = 00D7)
+
+// # These 3 should have some `name' assigned ... but what???
+
+// # Is XMWrap the right thing to wrap with (instead of XMArg)?
+// # We can't really assume that the stuff inside is sensible math.
+// # NOTE that \mathord and \mathbin aren't really right here.
+// # We need a finer granularity than TeX does: an ORD could be several things,
+// # a BIN could be a MULOP or ADDOP.
+// # AND, rarely, they're empty.... Is it wrong to drop them?
+// DefConstructor('\mathord{}', "?#1(<ltx:XMWrap role='ID'   >#1</ltx:XMWrap>)()", bounded => 1);
+// DefConstructor('\mathop{}', "?#1(<ltx:XMWrap role='BIGOP' scriptpos='#scriptpos'>#1</ltx:XMWrap>)()",
+//   bounded => 1, properties => { scriptpos => \&doScriptpos });
+// DefConstructor('\mathbin{}',   "?#1(<ltx:XMWrap role='BINOP'>#1</ltx:XMWrap>)()", bounded => 1);
+// DefConstructor('\mathrel{}',   "?#1(<ltx:XMWrap role='RELOP'>#1</ltx:XMWrap>)()", bounded => 1);
+// DefConstructor('\mathopen{}',  "?#1(<ltx:XMWrap role='OPEN' >#1</ltx:XMWrap>)()", bounded => 1);
+// DefConstructor('\mathclose{}', "?#1(<ltx:XMWrap role='CLOSE'>#1</ltx:XMWrap>)()", bounded => 1);
+// DefConstructor('\mathpunct{}', "?#1(<ltx:XMWrap role='PUNCT'>#1</ltx:XMWrap>)()", bounded => 1);
+// DefConstructor('\mathinner{}', "?#1(<ltx:XMWrap role='ATOM'>#1</ltx:XMWrap>)()",  bounded => 1);
+
+// # If an XMWrap (presumably from \mathop, \mathbin, etc)
+// # has multiple children, ALL are XMTok, within a restricted set of roles,
+// # we want to concatenate the text content into a single XMTok.
+// DefMathRewrite(xpath => 'descendant-or-self::ltx:XMWrap['
+//     # Only XMWrap's from the above class of operators
+//     . '(@role="OP" or @role="BIGOP" or @role="RELOP" '
+//     . 'or @role="ADDOP" or @role="MULOP" or @role="BINOP" '
+//     . 'or @role="OPEN" or @role="CLOSE")'
+//     . ' and count(child::*) > 1 '
+//     # with only XMTok as children with the roles in (roughly) the same set
+//     . ' and not(child::*[local-name() != "XMTok"])'
+//     . ' and not(ltx:XMTok['
+//     . '@role !="OP" and @role!="BIGOP" and @role!="RELOP" and @role!="METARELOP" '
+//     . 'and @role!="ADDOP" and @role!="MULOP" and @role!="BINOP" '
+//     . 'and @role!="OPEN" and @role!="CLOSE"'
+//     . '])]',
+//   replace => sub {
+//     my ($document, $node) = @_;
+//     my $replacement = $node->cloneNode(0);
+//     my $content     = $node->textContent;
+//     $replacement->appendText($content);
+//     $replacement->setName('ltx:XMTok');
+//     $document->getNode->appendChild($replacement);
+//   });
+
+// DefMacro('\hiderel{}', "#1");    # Just ignore, for now...
+
+// DefMathI('\to', undef, "\x{2192}", role => 'ARROW'); # RIGHTWARDS ARROW??? a bit more explicitly relation-like?
+
+// # TeX's ligatures handled by rewrite regexps.
+// # Note: applied in reverse order of definition (latest defined applied first!)
+// # Note also, these area only applied in text content, not in attributes!
+// DefPrimitive('\@@endash', sub { Box("\x{2013}", undef, undef, T_CS('\@@endash')); });
+// DefPrimitive('\@@emdash', sub { Box("\x{2014}", undef, undef, T_CS('\@@emdash')); });
+
+// DefLigature(qr{--}, "\x{2013}",
+//   fontTest => sub { $_[0]->getFamily ne 'typewriter'; }); # EN DASH (NOTE: With digits before & aft => \N{FIGURE DASH})
+// DefLigature(qr{---}, "\x{2014}",
+//   fontTest => sub { $_[0]->getFamily ne 'typewriter'; });    # EM DASH
+
+// # Ligatures for doubled single left & right quotes to convert to double quotes
+// # [should ligatures be part of a font, in the first place? (it is in TeX!)
+// DefLigature(qr{\x{2018}\x{2018}}, "\x{201C}",
+//   fontTest => sub { ($_[0]->getFamily ne 'typewriter')
+//       && (($_[0]->getEncoding || 'OT1') =~ /^(OT1|T1)$/); });    # is this needed?
+// DefLigature(qr{\x{2019}\x{2019}}, "\x{201D}",
+//   fontTest => sub { ($_[0]->getFamily ne 'typewriter')
+//       && (($_[0]->getEncoding || 'OT1') =~ /^(OT1|T1)$/); });
+
+  DefMacroI!(T_CS!("\\TeX"), None, Tokens::new(ExplodeText!("TeX")));
+  DefMacroI!(T_CS!("\\i"), None, T_OTHER!("\u{0131}")); // LATIN SMALL LETTER DOTLESS I
+  DefMacroI!(T_CS!("\\j"), None, T_OTHER!("\u{0237}"));
+
+// DefConstructor('\buildrel Until:\over {}',
+//   "<ltx:XMApp role='RELOP'>"
+//     . "<ltx:XMTok role='SUPERSCRIPTOP' scriptpos='#scriptpos'/>"
+//     . "<ltx:XMArg>#2</ltx:XMArg>"
+//     . "<ltx:XMArg>#1</ltx:XMArg>"
+//     . "</ltx:XMApp>",
+//   properties => { scriptpos => sub { "mid" . $_[0]->getBoxingLevel; } });
+
+  Ok(())
+}
