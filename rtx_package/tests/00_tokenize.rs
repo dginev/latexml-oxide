b258f66a114e0ae8742c24ea2935630e1db57438
Deyan Ginev <deyan.ginev@gmail.com>
2019-01-08 02:09:17 -0500

    move the last tokenize_todo test

--- rtx_package/tests/00_tokenize.rs
+++ rtx_package/tests/00_tokenize.rs
@@ -7,14 +7,5 @@ use rtx_package::util::test::*;
 #[test]
 fn can_tokenize() {
   let requires = None;
-
   rtx_tests("tests/tokenize", requires);
-}
-
-#[test]
-#[ignore]
-fn can_tokenize_todo() {
-  let requires = None;
-
-  rtx_tests("tests/tokenize_todo", requires);
 }
