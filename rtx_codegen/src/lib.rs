8cf131d78a8c9782916e72aac6f6481887ef3cd1
Deyan Ginev <d.ginev@jacobs-university.de>
2019-01-11 01:00:27 -0500

    feeling extremely close to automating inner state binding

--- rtx_codegen/src/lib.rs
+++ rtx_codegen/src/lib.rs
@@ -8,9 +8,8 @@ extern crate rtx_core;
 extern crate proc_macro;
 #[macro_use]
 extern crate quote;
-extern crate regex;
-extern crate syn;
 
+use crate::util::{get_option, get_options_from_input};
 use proc_macro::TokenStream;
 use syn::parse_macro_input;
 
@@ -63,5 +62,60 @@ pub fn derive_load_indirect_model(input: TokenStream) -> TokenStream {
       Err(e) => panic!("Failed to load indirect model: {:?}", e),
     },
     Err(e) => panic!("Failed to parse macro input: {:?}", e),
+  }
+}
+
+// May be good to track: https://github.com/rust-lang/rust/issues/54727
+// to see if it becomes possible one day to use this type of technique,
+// which would allow declarations such as:
+//     #[bound_state(outer)]
+//     pub fn load_definitions($state: &mut State, mut outer_stomach: Option<&mut Stomach>) -> Result<()> {
+//
+//     and
+//
+//     #[bound_state(inner)]
+//     | ... | { before digest closure here...}
+//
+//    making it possible to use the simple DefMacro("\\a","\\b") form in any context, while auto-binding the nearest state
+
+fn bug() -> ! { panic!("bug") }
+
+#[proc_macro_derive(BoundState, attributes(bind_options))]
+pub fn bound_state(input: TokenStream) -> TokenStream {
+  match parse_macro_input(&input.to_string()) {
+    Ok(item) => {
+      let options = get_options_from_input("bound_options", &item.attrs, bug);
+      let location_opt = options.as_ref().map(|o| get_option(&o, "location", bug));
+      let location = match location_opt {
+        Some(n) => {
+          println!("loc opt: {:?}", location_opt);
+          n
+        },
+        None => "outer",
+      };
+
+      let state_declaration = if location == "outer" {
+        quote!(
+          macro_rules! state {
+            () => {
+              outer_state!()
+            };
+          }
+        )
+      } else if location == "inner" {
+        quote!(
+          macro_rules! state {
+            () => {
+              inner_state!()
+            };
+          }
+        )
+      } else {
+        panic!("Unsupported bound state location: {:?}", location);
+      };
+
+      state_declaration.to_string().parse().unwrap()
+    },
+    Err(e) => panic!("Binding state failed: {:?}", e),
   }
 }
