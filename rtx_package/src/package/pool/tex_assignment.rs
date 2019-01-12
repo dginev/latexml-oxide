13f9eb299e9ac495affcee691b2f533d9e574319
Deyan Ginev <d.ginev@jacobs-university.de>
2019-01-12 16:43:56 -0500

    more precise Token equality impl

--- rtx_package/src/package/pool/tex_assignment.rs
+++ rtx_package/src/package/pool/tex_assignment.rs
@@ -127,13 +127,18 @@ LoadDefinitions!(state, {
   DefRegister!("\\catcode Number", Number::new(0.0),
     getter => Some(Rc::new(|args, state| {
       let num : f32 = args[0].to_number().value_of();
-      let code : Catcode = state.lookup_catcode((num as u8) as char).unwrap_or(Catcode::OTHER);
+      let refchar = (num as u8) as char;
+      // info!("-- looking up {:?}", refchar);
+      let code : Catcode = state.lookup_catcode(refchar).unwrap_or(Catcode::OTHER);
       let code : u8 = code.into();
+      // info!("-- code is: {:?}", code);
       Number::new(code).into()
     })),
     setter => Some(Rc::new(|value, args, state| {
       let c_char = (args[0].to_number().value_of() as u8) as char;
+      // info!("-- c_char: {:?}", c_char);
       let c_code = From::from(value.value_of() as u8);
+      // info!("-- c_code: {:?}", c_code);
       state.assign_catcode(c_char, c_code, None);
     }))
   );
