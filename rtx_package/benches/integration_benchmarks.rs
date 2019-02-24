7b1c9390381f43b4a31e8f720b5895aaf0f59b3d
Deyan Ginev <d.ginev@jacobs-university.de>
2019-02-24 18:57:24 -0500

    setup benchmarking via criterion, more to come as improvements land

---
+++ rtx_package/benches/integration_benchmarks.rs
@@ -0,0 +1,32 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+use rtx_core::{Core, CoreOptions};
+use rtx_package::core::DigestionAPI;
+
+/// Convenience function for iteratively executing rtx under a Bencher
+fn benchmark_texfile(c: &mut Criterion, tex_path: &'static str) {
+  let mut latexml = Core::new(CoreOptions {
+    verbosity: Some(-2),
+    search_paths: None,
+    preload: None,
+    include_comments: Some(false),
+    ..CoreOptions::default()
+  });
+  c.bench_function(tex_path, move |b| b.iter(||
+    latexml.convert_file(tex_path.to_string()).unwrap()
+  ));
+}
+
+fn bench_primes(c: &mut Criterion) {
+  benchmark_texfile(c, "tests/digestion/primes.tex");
+}
+
+fn bench_big_equality(c: &mut Criterion) {
+  benchmark_texfile(c, "tests/tokenize/equality.tex");
+}
+
+
+criterion_group!(benches, bench_primes, bench_big_equality);
+criterion_main!(benches);
