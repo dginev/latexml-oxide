55e3fce4ec8658e6d6be348d9e2368ee707f2f74
Deyan Ginev <d.ginev@jacobs-university.de>
2019-03-13 19:23:47 -0400

    precompile rawtex literals, yay!

--- rtx_package/src/package/setup_binding_language.rs
+++ rtx_package/src/package/setup_binding_language.rs
@@ -1773,10 +1773,21 @@ macro_rules! TokenizeInternal {
 
 #[macro_export]
 macro_rules! RawTeX {
+  ($text:literal) => {
+    bind_state_mut!(stmch,st);
+    let tokenized: Tokens;
+    compile_tokenize_internal!(tokenized, $text);
+    stmch.digest(tokenized, st)?;
+  };
   ($text:expr) => {
     bind_state_mut!(st);
     RawTeX!($text, st)
   };
+  ($text:literal, $state_arg:ident) => {{
+    let tokenized: Tokens;
+    compile_tokenize_internal!(tokenized, $text);
+    outer_stomach!().digest(tokenized, $state_arg)?;
+  }};
   ($text:expr, $state_arg:ident) => {{
     let mut state_stomach = $state_arg.stomach.clone();
     outer_stomach!().raw_tex($text, $state_arg)?;
