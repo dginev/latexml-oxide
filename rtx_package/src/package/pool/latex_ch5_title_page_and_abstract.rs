a79b23743a4b269af92484b547cdb69b62887161
Deyan Ginev <d.ginev@jacobs-university.de>
2019-03-14 18:20:19 -0400

    PASS structure/badabstract; def_dialect definitions still need more porting

--- rtx_package/src/package/pool/latex_ch5_title_page_and_abstract.rs
+++ rtx_package/src/package/pool/latex_ch5_title_page_and_abstract.rs
@@ -133,7 +133,9 @@ LoadDefinitions!(state, {
   locked => true);
   DefMacro!("\\abstract@onearg{}", "\\begin{abstract}#1\\end{abstract}");
 
-  DefMacro!("\\maybe@end@abstract", "\\endabstract");
+  DefMacro!("\\maybe@end@abstract", sub[gullet,args,state] {
+   T_CS!("\\endabstract")
+  });
 
   DefMacro!("\\abstractname", "Abstract");
   DefMacro!("\\format@title@abstract{}", "#1");
