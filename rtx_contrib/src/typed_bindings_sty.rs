6fcdd48cb01c1acc2ee4aff15c6e9b0410eea0a8
Deyan Ginev <deyan.ginev@gmail.com>
2023-01-27 11:17:52 -0500

    demo into

--- rtx_contrib/src/typed_bindings_sty.rs
+++ rtx_contrib/src/typed_bindings_sty.rs
@@ -1,14 +1,17 @@
 use rtx_package::*;
 
 LoadDefinitions!(state, {
-  TypedMacro!("\\sampler Number Token Dimension", sub[gullet, (number, token, dimension), _state] {
-    number.value_of();
-    dbg!(token);
-    dbg!(dimension);
-    Tokens!()
-  });
+  TypedMacro!("\\sampler Number Token Dimension",
+    sub[gullet, (number, token, dimension), _state] {
+      number.value_of();
+      dbg!(token);
+      dbg!(dimension);
+    });
 
   // DefMacro!("\\classic Number Token Dimension", sub[gullet, args, _state] {
   //   Tokens!()
   // });
 });
+
+
+// pub type Plain = Tokens;
