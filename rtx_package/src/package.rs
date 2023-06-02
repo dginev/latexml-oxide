8ef5994e4637c0f7811e901cdf52f20a5367ddb6
Deyan Ginev <deyan.ginev@gmail.com>
2023-06-02 18:22:55 -0400

    port over amsfonts.sty and amssymb.sty bindings

--- rtx_package/src/package.rs
+++ rtx_package/src/package.rs
@@ -172,6 +172,8 @@ pub mod pdftex;
 // Supported package bindings
 pub mod alltt_sty;
 pub mod amsmath_sty;
+pub mod amsfonts_sty;
+pub mod amssymb_sty;
 pub mod amsthm_sty;
 pub mod article_cls;
 pub mod cite_sty;
@@ -206,6 +208,8 @@ pub fn dispatch(filename: &str, stomach: &mut Stomach, state: &mut State) -> Opt
     "article.cls" => article_cls::load_definitions(stomach, state),
     "alltt.sty" => alltt_sty::load_definitions(stomach, state),
     "amsmath.sty" => amsmath_sty::load_definitions(stomach, state),
+    "amsfonts.sty" => amsfonts_sty::load_definitions(stomach, state),
+    "amssymb.sty" => amssymb_sty::load_definitions(stomach, state),
     "amsthm.sty" => amsthm_sty::load_definitions(stomach, state),
     "comment.sty" => comment_sty::load_definitions(stomach, state),
     "IEEEtran.cls" => ieeetran_cls::load_definitions(stomach, state),
---
+++ rtx_package/src/package/amsfonts_sty.rs
@@ -0,0 +1,53 @@
+use crate::package::*;
+use rtx_core::state::State;
+
+LoadDefinitions!(state, {
+//
+// See amsfndoc
+//
+DefConstructor!("\\mathbb{}", "#1", bounded => true, require_math => true, scope => Some(Scope::Global),
+  font => { family => "blackboard", series => "medium", shape => "upright" });
+DefMacro!("\\Bbb{}",  "\\mathbb{#1}");
+DefMacro!("\\bold{}", "\\mathbb{#1}");
+// Also defined in eufrak
+DefConstructor!("\\mathfrak{}", "#1", bounded => true, require_math => true, scope => Some(Scope::Global),
+  font => { family => "fraktur", series => "medium", shape => "upright" });
+DefMacro!("\\frak{}", "\\mathfrak{#1}");
+
+// Not necessarily math
+DefPrimitive!("\\checkmark",  "\u{2713}");    // CHECK MARK
+DefPrimitive!("\\circledR",   "\u{00AE}");     // REGISTERED SIGN
+DefPrimitive!("\\maltese",    "\u{2720}");    // MALTESE CROSs
+DefPrimitive!("\\yen",        "\u{00A5}");     // YEN SIGN
+
+// Math
+
+// These are delimiters, but open or close??
+DefMath!("\\ulcorner", "\u{231C}");    // TOP LEFT CORNER
+DefMath!("\\urcorner", "\u{231D}");    // TOP RIGHT CORNER
+DefMath!("\\llcorner", "\u{231E}");    // BOTTOM LEFT CORNER
+DefMath!("\\lrcorner", "\u{231F}");    // BOTTOM RIGHT CORNER
+
+DefMath!("\\dashrightarrow", "\u{21E2}", role => "ARROW");    // RIGHTWARDS DASHED ARROW
+DefMath!("\\dashleftarrow",  "\u{21E0}", role => "ARROW");    // LEFTWARDS DASHED ARROW
+DefMath!("\\dasharrow",      "\u{21E2}", role => "ARROW");    // RIGHTWARDS DASHED ARROW
+
+DefMath!("\\square",  "\u{25A1}");                            // WHITE SQUARE
+DefMath!("\\lozenge", "\u{25C6}");                            // WHITE DIAMOND
+
+DefMath!("\\vartriangleright", "\u{22B3}");                   // CONTAINS AS NORMAL SUBGROUP (\rhd)
+DefMath!("\\vartriangleleft",  "\u{22B2}");                   // NORMAL SUBGROUP OF (\lhd)
+
+DefMath!("\\trianglerighteq", "\u{22B5}");    // CONTAINS AS NORMAL SUBGROUP OR EQUAL TO (\unrhd)
+DefMath!("\\trianglelefteq",  "\u{22B4}");    // NORMAL SUBGROUP OF OR EQUAL TO (\unlhd)
+DefMath!("\\rightsquigarrow", "\u{219D}", role => "ARROW");    // RIGHTWARDS WAVE ARROW
+
+// amsfonts redefines various symbols already in TeX & LaTeX
+// \widehat, \widetilde, \rightleftharpoons,\angle
+// \hbar, \sqsubset, \sqsupset, \mho
+
+// amsfonts also redefines these, unless latexsym is loaded.
+// However, all these are already defined in TeX (from plain?)
+// \Box, \Diamond, \leadsto, \lhd, \unlhd, \rhd, \unrhd
+
+});
