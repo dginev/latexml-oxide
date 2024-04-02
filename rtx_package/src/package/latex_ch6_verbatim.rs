709320489c6c41546323007f4fcafdd75c6d6d2f
Deyan Ginev <deyan.ginev@gmail.com>
2024-04-02 19:45:45 -0400

    update verbatime to latest latexml support and tests

--- rtx_package/src/package/latex_ch6_verbatim.rs
+++ rtx_package/src/package/latex_ch6_verbatim.rs
@@ -1,18 +1,16 @@
 use crate::package::*;
 
-static END_VERBATIM_RE: Lazy<Regex> =
-  Lazy::new(|| Regex::new(r"^(.*?)\\end\{verbatim\}(.*?)$").unwrap());
 static SEMIVERBATIM_CHARS: [char;4] = ['%', '\\', '{', '}'];
 static T_OTHER_STAR: Lazy<Token> = Lazy::new(|| T_OTHER!("*"));
 
-//**********************************************************************
+//======================================================================
 // C.6.4 Verbatim
-//**********************************************************************
+//======================================================================
 #[rustfmt::skip]
 LoadDefinitions!({
   // NOTE: how's the best way to get verbatim material through?
-  DefEnvironment!("{verbatim}", "<ltx:verbatim>#body</ltx:verbatim>");
-  DefEnvironment!("{verbatim*}", "<ltx:verbatim>#body</ltx:verbatim>");
+  // DefEnvironment!("{verbatim}", "<ltx:verbatim>#body</ltx:verbatim>");
+  // DefEnvironment!("{verbatim*}", "<ltx:verbatim>#body</ltx:verbatim>");
 
   DefMacro!(
     "\\@verbatim",
@@ -20,7 +18,7 @@ LoadDefinitions!({
   ); // Close enough?
   DefConstructor!("\\lx@@verbatim", "<ltx:verbatim font='#font'>",
   before_digest => {
-   begin_semiverbatim(Some(&SEMIVERBATIM_CHARS));
+    begin_semiverbatim(Some(&SEMIVERBATIM_CHARS));
     merge_font(fontmap!(family => "typewriter", series => "medium", shape => "upright"));
     assign_catcode(' ', Catcode::ACTIVE, None);  // Do NOT (necessarily) skip spaces after \verb!!!
     Let!(&T_ACTIVE!(' '), T_SPACE!());
@@ -28,69 +26,22 @@ LoadDefinitions!({
   DefConstructor!(r"\lx@end@verbatim", "</ltx:verbatim>",
     before_digest => { end_semiverbatim()?; });
 
-  // Close enough?
   // verbatim is a bit of special case;
-  // It looks like an environment, but it only ends with an explicit "\end{verbatim}" on it's own
-  // line. So, we'll end up doing things more manually.
+  // It looks like an environment, but it only ends with an explicit "\end{verbatim}" on it's own line.
+  // So, we'll end up doing things more manually.
   // We're going to sidestep the Gullet for inputting,
   // and also the usual environment capture.
-
-  DefConstructor!(T_CS!("\\begin{verbatim}"), None, "<ltx:verbatim font='#font'>#body</ltx:verbatim>",
-    before_digest => {
-      bgroup();
-      let mut stuff = Vec::new();
-      if let Some(b) = state::lookup_tokens("@environment@verbatim@atbegin") {
-        stuff.push(stomach::digest(b.unlist())?);
-      }
-      AssignValue!("current_environment", "verbatim");
-      DefMacro!("\\@currenvir", "verbatim");
-      MergeFont!(family => "typewriter");
-      Ok(stuff)
-    },
-    after_digest => sub[whatsit] {
-      // makes you wonder if the `get_font` API should be working with Rc<Font> in the first place...
-      let font : Option<Rc<Font>> = whatsit.get_font()?.map(|ft| Rc::new((*ft).to_owned()));
-      let loc = whatsit.get_locator();
-      let mut lines : Vec<SymbolU32> = Vec::new();
-      while let Some(line) = gullet::read_raw_line() {
-        // The raw chars will still have to be decoded (but not space!!)
-        let mut decoded_line : String = String::new();
-        for c in line.chars() {
-          if c == ' ' { decoded_line.push(' '); }
-          else {
-            let decoded_c = font::decode_string(arena::pin_char(c), Some("OT1_typewriter"), true);
-            arena::with(decoded_c, |c_str| decoded_line.push_str(c_str));
-          }
-        }
-        if let Some(caps) = END_VERBATIM_RE.captures(&decoded_line) {
-          let pre = s!("{}\n", caps.get(1).map_or("", |m| m.as_str()));
-          let post = caps.get(2).map_or("", |m| m.as_str());
-          lines.push(arena::pin(pre));
-          let mut post_tokens = Tokenize!(post).unlist();
-          post_tokens.push(T_CR!());
-          gullet::unread(Tokens::new(post_tokens));
-          break;
-        } else {
-          lines.push(arena::pin(s!("{}\n", line)));
-        }
-      }
-      if let Some(last_line) = lines.last() {
-        if *last_line == arena::pin_static("\n") {
-          lines.pop();
-        }
-      }
-      // Note last line ends up as Whatsit's "trailer"
-      if let Some(b) = state::lookup_tokens("@environment@verbatim@atend") {
-        lines.push(arena::pin(stomach::digest(b)?.to_string()));
-      }
-      egroup()?;
-      lines.push(arena::pin_static("\\end{verbatim}"));
-      let boxes = lines.into_iter().map(|line|
-        Tbox::new(line, font.clone(), Some(loc),
-          Token{text: line, code:Catcode::OTHER}.into(), HashMap::default()).into()
-      ).collect();
-      whatsit.set_body(boxes);
-    },
+  DefConstructor!(T_CS!("\\begin{verbatim}"), None, 
+    "<ltx:verbatim font='#font'>#body</ltx:verbatim>",
+    before_digest => { before_digest_verbatim() }
+    after_digest => sub[whatsit] { after_digest_verbatim(false, whatsit)?; },
+    before_construct => sub[document, _whatsit] {
+      document.maybe_close_element("ltx:p")?; }
+  );
+  DefConstructor!(T_CS!("\\begin{verbatim*}"), None, 
+    "<ltx:verbatim font='#font'>#body</ltx:verbatim>",
+    before_digest => { before_digest_verbatim() }
+    after_digest => sub[whatsit] { after_digest_verbatim(true, whatsit)?; },
     before_construct => sub[document, _whatsit] {
       document.maybe_close_element("ltx:p")?; }
   );
@@ -108,17 +59,21 @@ LoadDefinitions!({
     // Do NOT (necessarily) skip spaces after \verb!!!
     assign_catcode(' ', Catcode::ACTIVE, None);
     let mut init = None;
+    let mut skipped_space = false;
     // As of texlive 2021, DO skip spaces before delimiter (even tho we've changed catcodes)
+    // but if we do skip spaces, * can be the delimiter
     let space_sym = arena::pin_static(" ");
     while let Some(maybe_init) = gullet::read_token()? {
-      if maybe_init.get_sym() != space_sym {
+      if maybe_init.get_sym() == space_sym {
+        skipped_space = true;
+      } else {
         init = Some(maybe_init);
         break;
       }
     }
     let mut starred = false;
     if let Some(ref init_token) = init {
-      if *init_token == *T_OTHER_STAR {
+      if *init_token == *T_OTHER_STAR && !skipped_space {
         starred = true;
         while let Some(maybe_init) =  gullet::read_token()? {
           if maybe_init.get_sym() != space_sym {
@@ -189,3 +144,68 @@ LoadDefinitions!({
   });
   DefMacro!(T_CS!("\\normalsfcodes"), None, Tokens!());
 });
+
+fn before_digest_verbatim() -> Result<Vec<Digested>> {
+  bgroup();
+  let mut stuff = Vec::new();
+  if let Some(b) = state::lookup_tokens("@environment@verbatim@atbegin") {
+    stuff.push(stomach::digest(b.unlist())?);
+  }
+  AssignValue!("current_environment", "verbatim");
+  DefMacro!("\\@currenvir", "verbatim");
+  MergeFont!(family => "typewriter");
+  Ok(stuff)
+}
+
+fn after_digest_verbatim(starred: bool, whatsit: &mut Whatsit) -> Result<()> { 
+  // makes you wonder if the `get_font` API should be working with Rc<Font> in the first place...
+  let font : Option<Rc<Font>> = whatsit.get_font()?.map(|ft| Rc::new((*ft).to_owned()));
+  let loc = whatsit.get_locator();
+  let (end,space) = if starred {
+    ("\\end{verbatim*}", '\u{2423}')
+  } else {
+    ("\\end{verbatim}", ' ')
+  };
+  let mut lines : Vec<SymbolU32> = Vec::new();
+  while let Some(next_line) = gullet::read_raw_line() {
+    let mut line = next_line.as_str();
+    let mut exiting = false;
+    if let Some((final_line,remaining)) = line.split_once(end) {
+      line = final_line;
+      gullet::unread_one(T_CR!());
+      gullet::unread(Tokenize!(remaining));
+      exiting = true;
+    }
+    // The raw chars will still have to be decoded (but not space!!)
+    let mut decoded_line : String = String::new();
+    for c in line.chars() {
+      if c == ' ' { decoded_line.push(space); }
+      else {
+        let decoded_c = font::decode_string(arena::pin_char(c), Some("OT1_typewriter"), true);
+        arena::with(decoded_c, |c_str| decoded_line.push_str(c_str));
+      }
+    }
+    decoded_line.push('\n');
+    lines.push(arena::pin(decoded_line));
+    if exiting {
+      break;
+    } 
+  }
+  if let Some(last_line) = lines.last() {
+    if *last_line == arena::pin_static("\n") {
+      lines.pop();
+    }
+  }
+  // Note last line ends up as Whatsit's "trailer"
+  if let Some(b) = state::lookup_tokens("@environment@verbatim@atend") {
+    lines.push(arena::pin(stomach::digest(b)?.to_string()));
+  }
+  egroup()?;
+  lines.push(arena::pin_static(end));
+  let boxes = lines.into_iter().map(|line|
+    Tbox::new(line, font.clone(), Some(loc),
+      Token{text: line, code:Catcode::OTHER}.into(), HashMap::default()).into()
+  ).collect();
+  whatsit.set_body(boxes);
+  Ok(())
+}
