378375129fde1caab7cc31c480826744f8e3b33d
dginev <dginev@users.noreply.github.com>
2024-11-13 12:16:24 +0000

    Automated deployment: Wed Nov 13 12:16:24 UTC 2024 e153e07cb50b9113c0b4fdde944d985594636eca

--- trait.impl/core/ops/index/trait.Index.js
+++ trait.impl/core/ops/index/trait.Index.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["latexml_core",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;&amp;<a class=\"struct\" href=\"https://docs.rs/crate/string-interner/0.12.0/string_interner/symbol/struct.SymbolU32.html\" title=\"struct string_interner::symbol::SymbolU32\">SymbolU32</a>&gt; for <a class=\"struct\" href=\"latexml_core/common/arena/data/struct.SymHashMap.html\" title=\"struct latexml_core::common::arena::data::SymHashMap\">SymHashMap</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["latexml_core",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;&amp;<a class=\"struct\" href=\"https://docs.rs/crate/string-interner/0.18.0/string_interner/symbol/struct.SymbolU32.html\" title=\"struct string_interner::symbol::SymbolU32\">SymbolU32</a>&gt; for <a class=\"struct\" href=\"latexml_core/common/arena/data/struct.SymHashMap.html\" title=\"struct latexml_core::common::arena::data::SymHashMap\">SymHashMap</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
