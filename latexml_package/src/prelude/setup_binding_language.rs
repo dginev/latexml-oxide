eea74584ce33b57a6d2f3193c45c2fd8cbf44f9b
deyan <deyan.ginev@gmail.com>
2024-11-22 14:41:16 -0500

    re-pass all ported tests, upto Unicode math PR #2425

--- latexml_package/src/prelude/setup_binding_language.rs
+++ latexml_package/src/prelude/setup_binding_language.rs
@@ -779,7 +779,9 @@ macro_rules! Expand {
 
 #[macro_export]
 macro_rules! Input {
-  ($arg:expr) => { ::latexml_core::binding::content::input($arg, InputOptions::default())? }
+  ($arg:expr) => {
+    ::latexml_core::binding::content::input($arg, InputOptions::default())?
+  };
 }
 
 // /// Return `tokens` with all partial expandsion
