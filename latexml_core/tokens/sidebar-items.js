f6ea01b117b63924329ed92c274bfc150602058f
dginev <dginev@users.noreply.github.com>
2026-07-27 04:42:27 +0000

    Automated deployment: Mon Jul 27 04:42:26 UTC 2026 2fa6f4256b0a78b1f893602d3eb7fc6a25d6bf49

--- latexml_core/tokens/sidebar-items.js
+++ latexml_core/tokens/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["NO_BORROWED_TOKENS","NO_TOKENS","UNTEX_LINELENGTH"],"struct":["Tokens"]};
+window.SIDEBAR_ITEMS = {"constant":["NO_BORROWED_TOKENS","NO_TOKENS","UNTEX_LINELENGTH"],"struct":["TeXString","Tokens"]};
--- trait.impl/core/convert/trait.AsRef.js
+++ trait.impl/core/convert/trait.AsRef.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["latexml_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"latexml_core/tokens/struct.Tokens.html\" title=\"struct latexml_core::tokens::Tokens\">Tokens</a>&gt; for <a class=\"struct\" href=\"latexml_core/tokens/struct.Tokens.html\" title=\"struct latexml_core::tokens::Tokens\">Tokens</a>",0]]]]);
+    const implementors = Object.fromEntries([["latexml_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"latexml_core/tokens/struct.Tokens.html\" title=\"struct latexml_core::tokens::Tokens\">Tokens</a>&gt; for <a class=\"struct\" href=\"latexml_core/tokens/struct.Tokens.html\" title=\"struct latexml_core::tokens::Tokens\">Tokens</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"latexml_core/tokens/struct.TeXString.html\" title=\"struct latexml_core::tokens::TeXString\">TeXString</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[429]}
+//{"start":59,"fragment_lengths":[821]}
